44f97861a6571337179d2b8acf890111169f49c3
Diogo Gonçalves <goncalveshd@gmail.com>
2014-10-14 09:57:08 -0300

    Add config option to set blog name

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 # Dirty blogger configuration file
 
+# Set your blog name
+BLOG_NAME = 'Dirtyblog'
+
 # Set your trackingId on Google Analytics create method.
 # More info on: https://developers.google.com/analytics/devguides/collection/analyticsjs/method-reference#create
 GOOGLE_ANALYTICS_TRACKING_ID = 'UA-XXXXXXXX-X'
@@ -7,10 +10,6 @@ GOOGLE_ANALYTICS_TRACKING_ID = 'UA-XXXXXXXX-X'
 # Set your data-url on Tweet Button.
 # More info on: https://dev.twitter.com/web/tweet-button
 TWITTER_DATA_URL = 'http://www.your-web-site-here.net'
-
-# Set your data-href on +1 Button.
-# More info on: https://developers.google.com/+/web/+1button/
-GOOGLE_PLUS_DATA_HREF = 'http://www.your-web-site-here.net'
 
 # Set your data-href on Facebook Like Button.
 # More info on: https://developers.facebook.com/docs/plugins/like-button/
